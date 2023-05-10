51a60d8d04e91cfadd6964172235b0dbf18cf0bd
valuvilla <valuvp@hotmail.com>
2023-05-10 20:00:31 +0200

    porque no me fucniona

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,7 +12,7 @@ se consideraran anteúltimo y último respectivamente en el orden alfabético;
 descomprimir los siguientes mensajes –cuyo árbol ha sido construido de la misma manera que el ejemplo visto anteriormente:
 """
 
-from tda_arbol_bin import crear_arbol_huffman
+from tda_arbol_bin import Arbol
 # Cargamos los datos con las frecuencias
 tabla = [
     ['A',0.098],
@@ -42,40 +42,10 @@ frecuencia = {}
 for i in tabla:
     frecuencia[i[0]] = i[1]
 
-print(frecuencia)
+# Creamos el arbol
+arbol = Arbol.construir_arbol_huffman(frecuencia)
 
-def decodificar(cadena, arbol_huff):
-    cadena_deco = ''
-    raiz_aux = arbol_huff
-    for bit in cadena:
-        if bit == "0":
-            raiz_aux = raiz_aux.izq
-        elif bit == "1":
-            raiz_aux = raiz_aux.der
-    return cadena_deco
+# Decodificamos el mensaje
+mensaje="10001011101011000010111010001110000011011000000111100111101001011000011010011100110100010111010111111101000011110011111100111101000110001100000010110101111011111110111010110110111001110110111100111111100101001010010100000101101011000101100110100011100100101100001100100011010110101011111111111011011101110010000100101011000111111100010001110110011001011010001101111101011010001101110000000111001001010100011111100001100101101011100110011110100011000110000001011010111110011100"
 
-
-def codificar_caracter(raiz, caracter, codigo_actual):
-        if raiz == None:
-            return None
-        if raiz.valor == caracter:
-            return codigo_actual
-        codigo_izq = codificar_caracter(raiz.izq, caracter, codigo_actual + "0")
-        if codigo_izq != None:
-            return codigo_izq
-        codigo_der = codificar_caracter(raiz.der, caracter, codigo_actual + "1")
-        return codigo_der
-
-
-def codificar(cadena, arbol_huff):
-    cadena_cod = ''
-    for caracter in cadena:
-        cadena_cod += codificar_caracter(arbol_huff, caracter, '')
-    return cadena_cod
-
- 
-cadena_codificada="10001011101011000010111010001110000011011000000111100111101001011000011010011100110100010111010111111101000011110011111100111101000110001100000010110101111011111110111010110110111001110110111100111111100101001010010100000101101011000101100110100011100100101100001100100011010110101011111111111011011101110010000100101011000111111100010001110110011001011010001101111101011010001101110000000111001001010100011111100001100101101011100110011110100011000110000001011010111110011100"
-cadena_decodificada = decodificar(cadena_codificada, crear_arbol_huffman(frecuencia))
-print(cadena_decodificada)
-
-
+decodificado = Arbol.decodificar_mensaje(mensaje, arbol)
