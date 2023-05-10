8cabe37aca0d91829e144c531f0b6a2c906bf896
valuvilla <valuvp@hotmail.com>
2023-05-10 16:29:10 +0200

    cambios

---
+++ Ejercicio4.py
@@ -0,0 +1,15 @@
+"""
+Desarrollar un algoritmo numérico iterativo que permita 
+calcular el método de la bisección de una función f(x).
+
+Desarrollar un algoritmo numérico iterativo que permita 
+calcular el método de la secante de una función f(x).
+
+Desarrollar un algoritmo numérico iterativo que permita 
+calcular el método de Newton-Raphson de una función f(x).
+
+Comparar los tres algoritmos anteriores para resolver 
+la siguiente función: x3 + x +16 = 0, 
+respecto de la cantidad de iteraciones necesarias por 
+cada método para converger. ¿Cuánto es la diferencia en decimales
+ entre las distintas soluciones?"""
