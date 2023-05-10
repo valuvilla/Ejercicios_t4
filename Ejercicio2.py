862643521f46e06660c49d828c8b9a58e11fc8ce
valuvilla <valuvp@hotmail.com>
2023-05-10 16:26:44 +0200

    ca,bios

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -32,24 +32,98 @@ pedidos de misiones durante la atención;
 indicar la cantidad total de recursos asignados a las misione
 """
 
+import random
+
+
 class Misiones(object):
     def __init__(self, tipo, planeta, general):
         self.tipo = tipo
         self.planeta = planeta
         self.general = general
-        self.sig = None
-
+        self.recursos = {}
 
 class GrafoMisiones(object):
     def __init__(self):
         self.inicio = None
         self.tamanio = 0
 
-def insertar_vertice(grafo, dato):
-    """Inserta un dato como vértice en el grafo."""
-    nodo = Misiones(dato)
+
+def insertar_misiones(grafo, mision):
+    nodo = Misiones(mision.tipo, mision.planeta, mision.general)
     nodo.sig = grafo.inicio
     grafo.inicio = nodo
     grafo.tamanio += 1
 
-def
+
+def prioridad(mision):
+    # Si el general es Palpatine o Darth Vader, la prioridad es alta
+    if mision.general == "Palpatine" or mision.general == "Darth Vader":
+        return "Alta"
+    else:
+        return "Baja"
+    
+def asignar_recursos(mision):
+    vehículos = ["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST"]
+    prioridad_mision = prioridad(mision)
+    if prioridad_mision == "Alta":
+        # Asignar recursos manualmente
+        print("Asignar recursos manualmente")
+    else:
+        # Asignar recursos automaticamente
+        if mision.tipo == "Exploracion":
+            mision.recursos = {"Scout Troopers": 15, "Speeder Bike": 2}
+        elif mision.tipo == "Contencion":
+            mision.recursos = {"Stormtroopers": 30}
+            for i in range(3):
+                mision.recursos[random.choice(vehículos)] = 1
+        elif mision.tipo == "Ataque":
+            mision.recursos = {"Stormtroopers": 50}
+            for i in range(7):
+                mision.recursos[random.choice(vehículos)] = 1
+    return mision.recursos
+
+def mostrar_todo(mision):
+    total_recursos = {}
+    for i in range(mision.tamanio):
+        print("Tipo: ", mision.tipo)
+        print("Prioridad: ", prioridad(mision))
+        print("Planeta: ", mision.planeta)
+        print("General: ", mision.general)
+        print("Recursos: ", mision.recursos)
+        for recurso in mision.recursos:
+            if recurso in total_recursos:
+                total_recursos[recurso] += mision.recursos[recurso]
+            else:
+                total_recursos[recurso] = mision.recursos[recurso]
+        mision = mision.sig
+    print("Total de recursos: ", total_recursos)
+
+
+# Experimentacion
+m1= Misiones("Exploracion", "Tatooine", "Palpatine")
+m2= Misiones("Contencion", "Naboo", "Darth Vader")
+m3= Misiones("Ataque", "Alderaan", "General Grievous")
+m4= Misiones("Exploracion", "Kashyyyk", "General Grievous")
+m5= Misiones("Contencion", "Kamino", "Palpatine")
+m6= Misiones("Ataque", "Geonosis", "Darth Vader")
+
+grafo = GrafoMisiones()
+
+insertar_misiones(grafo, m1)
+insertar_misiones(grafo, m2)
+insertar_misiones(grafo, m3)
+insertar_misiones(grafo, m4)
+insertar_misiones(grafo, m5)
+insertar_misiones(grafo, m6)
+
+# Asignar recursos
+mision = grafo.inicio
+for i in range(grafo.tamanio):
+    mision.recursos = asignar_recursos(mision)
+    mision = mision.sig
+
+# Mostrar todo
+mostrar_todo(grafo)
+
+    
+    
