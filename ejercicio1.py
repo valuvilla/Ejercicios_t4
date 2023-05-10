4bf24d62a10cdde1efde23a5a79acd9501d57519
valuvilla <valuvp@hotmail.com>
2023-05-10 15:43:21 +0200

    cambios

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 1
+Desarrollar los algoritmos necesarios para generar un árbol de Huffman 
+a partir de la siguiente tabla –para lo cual deberá calcular primero las 
+para resolver las siguientes actividades:
+
+1. la generación del árbol debe hacerse desde los caracteres 
+de menor frecuencia hasta los de mayor, en el caso de que dos caracteres tengan la misma frecuencia, 
+primero se toma el que este primero en el alfabeto, el carácter “espacio” y “coma” 
+se consideraran anteúltimo y último respectivamente en el orden alfabético;
+ 
+descomprimir los siguientes mensajes –cuyo árbol ha sido construido de la misma manera que el ejemplo visto anteriormente:
+"""
